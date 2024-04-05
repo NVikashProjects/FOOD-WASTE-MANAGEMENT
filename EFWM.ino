7f273168ec9ea2d87ce3d02ad64a6e9239caf8d4
NVikashProjects <165534119+NVikashProjects@users.noreply.github.com>
2024-04-05 06:32:14 +0530

    Update EFWM.ino

--- EFWM.ino
+++ EFWM.ino
@@ -10,42 +10,69 @@
 #include "HX711.h"
 
 #define calibration_factor -7050.0
-#define LOADCELL_DOUT_PIN  3
-#define LOADCELL_SCK_PIN  2
+#define LOADCELL_DOUT_PIN 3
+#define LOADCELL_SCK_PIN 2
+#define CAMARA 14
 
 HX711 scale;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-char auth[] = BLYNK_AUTH_TOKEN; 
-char ssid[] = "EFWM";        
-char pass[] = "12345678";    
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "EFWM";
+char pass[] = "12345678";
+
+float AfterWeight, currentWeight = 0.0;
+int personCount = 0;
 
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 
-  lcd.begin(16,2);
+  pinMode(CAMARA, OUTPUT);
+
+  lcd.begin(16, 2);
   lcd.init();
   lcd.backlight();
-  
+
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(calibration_factor); 
+  scale.set_scale(calibration_factor);
 }
 
 void loop() {
-  float weight = map(scale.get_units(), -70, -200, 0, 5); 
+  float weight = map(scale.get_units(), -70, -200, 0.0, 5.0);
+  float millweight = map(scale.get_units(), -76, -200, 0.0, 5000.0);
   Serial.print("Food Waste Quantity : ");
   Serial.print(weight, 1);
-  Serial.println(" kg");
-  
+  Serial.println(" kg   ");
+  Serial.print(millweight, 1);
+  Serial.println(" gram");
+
   lcd.clear();
-  lcd.setCursor(2, 0);
+  lcd.setCursor(0, 0);
   lcd.print("Food Waste: ");
-  lcd.print(weight, 0);
+  lcd.print(millweight, 1);
   lcd.print(" kg");
-  lcd.noBlink();
+
+  lcd.setCursor(0, 1);
+  lcd.print("Persons : ");
+  lcd.print(personCount);
   
-  Blynk.virtualWrite(V1, weight); 
-  delay(1000);
+  lcd.noBlink();
+
+  if ( millweight > currentWeight) {
+    AfterWeight = millweight - currentWeight;
+
+    if (AfterWeight > 200.0) {
+      personCount += 1;
+      digitalWrite(CAMARA, HIGH);
+      delay(500);
+      digitalWrite(CAMARA, LOW);
+    }
+    currentWeight = millweight;
+  }
+
+  Blynk.virtualWrite(V2, personCount);
+  Blynk.virtualWrite(V1, weight);
+  delay(1500);
 }
